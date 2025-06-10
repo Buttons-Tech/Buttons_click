7137d42f92397d0a508c586c56078187f078116f
unknown <buttonsafrotech@gmail.com>
2025-06-10 19:41:20 +0100

    services

---
+++ app/components/service.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Ourservice = () => {
+  return (
+    <>
+    <div className='w-screen h-[606px] '>
+        <h1 className='text-center font  font-extrabold text-[60px] text-[#543A2F] pb-6 '>Our Services</h1>
+        <h3 className='text-center font-medium text-[28px]'>From concept to deployment, we provide end-to-end solutions that empower your <br /> business to thrive in a digital world.</h3>
+    </div>
+    </>
+  )
+}
+
+export default Ourservice
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,15 @@ import Image from "next/image";
 import Nav from "./components/nav";
 import Button from "./components/button";
 import Hero from "./components/hero";
+import Ourservice from "./components/service";
 
 export default function Home() {
   return (
    <>
    
    <Nav />
-   
    <Hero />
+   <Ourservice />
    </>
-      
-    
   );
 }
