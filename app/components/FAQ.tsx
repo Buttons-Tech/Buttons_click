619ed68be390fef1a3236b041bcea7690e5f79b5
Ohakwechi <chigozieohakwej@gmail.com>
2025-07-02 11:50:02 +0100

    switched devies

--- app/components/FAQ.tsx
+++ app/components/FAQ.tsx
@@ -12,8 +12,10 @@ const FAQ = () => {
             <h2 className='font font-bold text-[44px] text-[#333333] pb-6'>Frequently Asked Questions</h2>
             <Image src={faq} alt="" width={657} height={487} className="w-[657px] h-[487px] " />
         </div>
-        <div className='flex-row pt-9'>
+        <div className='
+        flex-row pt-9 gap-5'>
             <button className='bg-[#EBCCB2] h-[85px] w-[593px] rounded-[10px] flex justify-between test-[24px] font-semibold py-8 px-6'>What is Buttons? <Image src={down} alt="" width={30} height={20} className='w-[48px] h-[48.77px] pb-2 ' /></button>
+            <button className='bg-[#EBCCB2] h-[85px] w-[593px] rounded-[10px] flex justify-between test-[24px] font-semibold py-8 px-6'>What do i need to bring my idea to life? <Image src={down} alt="" width={30} height={20} className='w-[48px] h-[48.77px] pb-2 ' /></button>
         </div>
 
     </div>
