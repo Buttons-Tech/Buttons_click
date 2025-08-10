50c1007026954f34993b9e9817ce5a3bcb06667c
Buttons Technology <buttonsafrotech@gmail.com>
2025-08-10 07:57:51 +0100

    dreambox-hero-header

---
+++ app/components/Dream/HeroHeader.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeroHeader = () => {
+  return (
+    <>
+        <div className='text-[10px] flex flex-col leading-[19px] p-[29px]'>
+            <span className='text-[32px] font-black text-[#7769B6] '>EARLY YEARS</span><br />
+            <span className='text-[32px] font-bold'>Investing Early in </span> <br />
+            <span className='text-[32px] font-bold text-[#FFBE15]'>Their Future </span><br />
+            <span className=''>Our e-learning programs have been developed to be a vehicle of delivering creative learning solutions for your toddlers </span>
+        </div>
+    </>
+  )
+}
+
+export default HeroHeader
--- app/dreambox/page.tsx
+++ app/dreambox/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import NavBar from '../components/Dream/NavBar'
 import Hero from '../components/Dream/Hero'
+import HeroHeader from '../components/Dream/HeroHeader'
 
 const page = () => {
   return (
     <>
-    <div className='bg-[#D0CFCD] w-screen h-screen p-12'>
+    <div className='bg-[#D0CFCD] w-screen p-12'>
 
       <NavBar/>
       <Hero/>
+      <HeroHeader/>
     </div>
     </>
   )
